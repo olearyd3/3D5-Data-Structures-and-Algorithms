9cfc54e650890ff3268ec5d5c83adcb5834bf944
olearyd3 <66378588+olearyd3@users.noreply.github.com>
2021-11-23 12:46:44 +0000

    Create bstdb.h

---
+++ Assignment3-searching-algorithms/task2/src/bstdb.h
@@ -0,0 +1,11 @@
+#ifndef BSTDB_H
+#define BSTDB_H
+
+int   bstdb_init           ( void );
+int   bstdb_add            ( char *name, int word_count );
+int   bstdb_get_word_count ( int doc_id );
+char* bstdb_get_name       ( int doc_id );
+void  bstdb_stat           ( void );
+void  bstdb_quit           ( void );
+
+#endif
